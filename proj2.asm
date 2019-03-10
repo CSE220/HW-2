56b0fdf56d5cc37b1a095eb6258efdb276c02a97
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-03-10 15:29:47 -0400

    Implemented character count

--- proj2.asm
+++ proj2.asm
@@ -2,11 +2,8 @@
 # ssivakumaran
 # 110261379
 
-#################### DO NOT CREATE A .data SECTION ####################
-#################### DO NOT CREATE A .data SECTION ####################
-#################### DO NOT CREATE A .data SECTION ####################
-
 .text
+#################### TO LOWERCASE ####################
 to_lowercase:
 	move $t0, $a0		# Set $t0 to be the base address of the string
 	li $v0, 0		# Initialize return value to 0
@@ -39,13 +36,30 @@ to_lowercase.uppercase_check:
 to_lowercase.not_letter:
 	addi $t0, $t0, 1		# Move read adddress up by 1
 	j to_lowercase.loop
-return:
-	jr $ra
-
+#################### STRING LENGTH ####################
 strlen:
-    jr $ra
+    	move $t0, $a0		# Set $t0 to be the base address of the string
+	li $v0, 0		# Initialize return value to 0
 	
-
+	addi $sp, $sp, -4	# Allocates space on stack
+	sw $s0, 0($sp)		# Saved $s0 onto stack
+	move $s0, $ra		# Move $ra value to be saved
+	jal strlen.loop
+	move $ra, $s0		# Restore $ra value
+	lw $s0, 0($sp)		# Restore $s0 value
+	addi $sp, $sp, 4	# Allocates space on stack
+	
+	j return
+strlen.loop:
+	lbu $t1, 0($t0)		# Load character into $t2
+	
+	beq $t1, 0, return	# If character is 0, we are done
+	
+	addi $v0, $v0, 1	# Increment return value by 1
+	addi $t0, $t0, 1	# Move read address up by 1
+	j strlen.loop
+	
+#################### LETTER COUNT ####################
 count_letters:
 	jr $ra
 
@@ -61,10 +75,10 @@ encrypt:
 decode_ciphertext:
 	jr $ra
 	
-	
+
 decrypt:
 	jr $ra
-
-#################### DO NOT CREATE A .data SECTION ####################
-#################### DO NOT CREATE A .data SECTION ####################
-#################### DO NOT CREATE A .data SECTION ####################
+	
+#################### UTILS ####################
+return:
+	jr $ra
